3bf48c2afe9acd3775d0c6fe0bf56085b96e288b
sabrinaprc <sabrinyp@uci.edu>
2025-03-22 20:36:04 -0700

    Added word count field to the database

--- src/mongodb_utils.py
+++ src/mongodb_utils.py
@@ -6,9 +6,12 @@ from collections import defaultdict
 
 client = MongoClient("mongodb://localhost:27017/")
 db = client["search_engine"]
+
 url_collection = db["doc_urls"]
+index_collection = db["inverted_index"]
 
 url_collection.drop()
+url_collection.create_index("doc_id", unique=True)
 
 with open("../doc_id_to_url.json", "r") as f:
     mapping = json.load(f)
@@ -17,6 +20,30 @@ for doc_id, url in mapping.items():
     doc = {"doc_id": int(doc_id), "url": url}
     url_collection.insert_one(doc)
     print(f"Inserted doc_id: {doc_id}")
+    
+    
+doc_index = defaultdict(float)
+for entry in index_collection.find():
+    for posting in entry["postings"]:
+        doc_id = posting["doc_id"]
+        doc_index[doc_id] += posting["frequency"]
+
+for doc_id, word_count in doc_index.items():
+    result = url_collection.update_one(
+        {"doc_id": doc_id},
+        {"$set": {"word_count": word_count}}
+    )
+    print(f"Updated doc_id {doc_id} with word_count = {word_count}")
+
+# url_collection.drop()
+
+# with open("../doc_id_to_url.json", "r") as f:
+#     mapping = json.load(f)
+
+# for doc_id, url in mapping.items():
+#     doc = {"doc_id": int(doc_id), "url": url}
+#     url_collection.insert_one(doc)
+#     print(f"Inserted doc_id: {doc_id}")
     
 
 # collection = db["inverted_index"]
